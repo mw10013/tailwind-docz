b2e6f55beac9f005ca47953e33cec583a31caf0a
Michael Wu <mw10013@gmail.com>
2021-11-01 11:59:34 -0400

    cleanup wrapper.js

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -5,9 +5,6 @@ const Wrapper = ({ children, doc }) => {
   return (
     <>
       <Helmet>
-        <meta charSet="utf-8" />
-        {/* <title>{doc.value.name}</title> */}
-        {/* <title>Tailwind Docz</title> */}
         <link
           href="https://unpkg.com/tailwindcss@2/dist/tailwind.min.css"
           rel="stylesheet"
