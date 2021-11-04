5f89f732f94063a32bf2d9024887cf7568ca5da1
Michael Wu <mw10013@gmail.com>
2021-11-04 11:26:06 -0400

    created button

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type ButtonProps = {
+  children?: React.ReactNode;
+};
+
+export const Button = ({ children }: ButtonProps) => {
+  return (
+    <button className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      {children}
+    </button>
+  );
+};
