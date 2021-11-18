ec9bde53735bad77009c31e77723eb9d9225b587
Michael Wu <mw10013@gmail.com>
2021-11-18 13:34:33 -0500

    wip select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -39,7 +39,7 @@ export const TWSelectSandbox = () => {
       <select
         id="location"
         name="location"
-        className="border-none focus:border-transparent focus:ring-transparent mt-1 block w-full pl-3 pr-10 py-2 text-base sm:text-sm"
+        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         defaultValue="Canada"
       >
         <option>United States</option>
